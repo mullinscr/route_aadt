0505736f90aeb80571611df7deb061a69157fa4b
Callum Mullins <mullinscr@gmail.com>
2023-05-23 20:23:29 +0100

    Prints initial results

--- dockwidget_ui.py
+++ dockwidget_ui.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 import processing
-from qgis.core import QgsProject, QgsGraduatedSymbolRenderer, QgsStyle, QgsVectorLayerSimpleLabeling, QgsPalLayerSettings, QgsTextFormat, edit
+from qgis.core import QgsProject, QgsGraduatedSymbolRenderer, QgsStyle, QgsVectorLayerSimpleLabeling, QgsPalLayerSettings, QgsTextFormat, edit, QgsGeometry
 from qgis.PyQt.uic import loadUiType
 from qgis.PyQt.QtWidgets import QDockWidget
 from qgis.PyQt.QtGui import QFont
@@ -15,15 +15,16 @@ class DockwidgetUI(QDockWidget, FORM_CLASS):
     
         self.btnViewADTSites.clicked.connect(self.load_adt_sites)
         self.btnRemoveSites.clicked.connect(self.remove_selected_sites)
+        self.btnGenerateResult.clicked.connect(self.generate_result)
 
     def load_adt_sites(self):
-        route_lyr = self.cmbxRouteLyr.currentLayer()
-        adt_lyr =  self.cmbxADTLyr.currentLayer()
-        buff_dist = self.spbxBufferM.value()
+        self.route_lyr = self.cmbxRouteLyr.currentLayer()
+        self.adt_lyr =  self.cmbxADTLyr.currentLayer()
+        self.buff_dist = self.spbxBufferM.value()
 
         buffered = processing.run("native:buffer",
-            {'INPUT':route_lyr,
-            'DISTANCE':buff_dist,
+            {'INPUT':self.route_lyr,
+            'DISTANCE':self.buff_dist,
             'SEGMENTS':120,
             'END_CAP_STYLE':0,
             'JOIN_STYLE':0,
@@ -33,12 +34,12 @@ class DockwidgetUI(QDockWidget, FORM_CLASS):
             )['OUTPUT']
 
         intersecting_adt = processing.run("native:extractbylocation",
-            {'INPUT':adt_lyr,
+            {'INPUT':self.adt_lyr,
             'PREDICATE':[0],
             'INTERSECT':buffered,
             'OUTPUT':'memory:'})['OUTPUT']
 
-        intersecting_adt.setName(f'ADT sites ({buff_dist} m buffer): {route_lyr.sourceName()}')
+        intersecting_adt.setName(f'ADT sites ({self.buff_dist} m buffer): {self.route_lyr.sourceName()}')
         
         # style layer
         graduated_renderer = QgsGraduatedSymbolRenderer()
@@ -77,3 +78,50 @@ class DockwidgetUI(QDockWidget, FORM_CLASS):
 
     def merge_selected_sites(self):
         ...
+    
+    def generate_result(self):
+        # create a total route geometry
+        vertices = []
+        feats = []
+        for feat in self.route_lyr.getFeatures():
+           feats.append(feat)
+        feats = sorted(feats, key=lambda x: x['fid']) # expects route builder output
+        
+        for feat in feats:
+            vertices.extend([v for v in feat.geometry().vertices()])
+        route_geom = QgsGeometry.fromPolyline(vertices)
+
+        route_length = route_geom.length() * 2 # times 2 as drawn single dir
+        distances = []
+        adts = [] 
+        throughput = 0
+
+        for site in self.adt_sites.getFeatures():
+            geom = site.geometry()
+            # transform to 27700
+            adt = site['7 day']
+            dist = route_geom.lineLocatePoint(geom)
+            distances.append(dist)
+            adts.append(adt)
+
+        r = zip(distances, adts)
+        r = sorted(r, key=lambda x: x[0])
+
+        import numpy as np
+        print(np.mean(adts), np.median(adts))
+
+        for n in range(1, len(r) - 1):
+            d_ahead = (r[n + 1][0] + r[n][0]) / 2
+            d_behind = (r[n][0] + r[n - 1][0]) / 2
+            d = d_ahead - d_behind
+            throughput += (d * 2 * r[n][1]) # * 2 as routes are drawn single dir
+
+        # first site
+        d = (r[1][0] + r[0][0]) / 2
+        throughput += (d * 2 * r[0][1])
+
+        # last site
+        d = route_geom.length() - ((r[-1][0] + r[-2][0]) / 2)
+        throughput += (d * 2 * r[-1][1])
+
+        print(f'THROUGHPUT (km): {(throughput / 1000):,.0f}\nADT: {(throughput / route_length):,.0f}')
